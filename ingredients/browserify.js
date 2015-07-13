446bc68ee5fdbf199975cd4511402e26f3deb8d0
Mohamed Boudra <boudra.moha@gmail.com>
2015-07-13 06:29:28 +0200

    enable customizing browserify transforms

--- ingredients/browserify.js
+++ ingredients/browserify.js
@@ -38,10 +38,13 @@ var getDestination = function(output) {
  * @param {object}       options
  */
 var browserifyStream = function(data) {
-    var stream = browserify(data.src, data.options);
 
-    stream.transform(babelify, { stage: 0 });
-    stream.transform(partialify);
+    data.options.transform = data.options.transform || [
+        [babelify, { stage: 0 }],
+        partialify
+    ];
+
+    var stream = browserify(data.src, data.options);
 
     return stream;
 };
